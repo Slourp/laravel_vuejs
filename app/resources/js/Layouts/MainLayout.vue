b2780777f445688ae4e2c84786ff09a57da0e8b4
David VANMAK <vanmak.david.lwg@gmail.com>
2023-07-31 16:24:12 +0200

    [ADD] reactive data

--- app/resources/js/Layouts/MainLayout.vue
+++ app/resources/js/Layouts/MainLayout.vue
@@ -1,9 +1,16 @@
 <template>
     <Link href="/">Main Page</Link>
     <Link href="/show">Show Page</Link>
+    <div>
+        <p>The Page with time {{ timer }}</p>
+    </div>
     <slot></slot>
 </template>
 
 <script setup lang="ts">
 import { Link } from "@inertiajs/vue3";
+import { ref } from "vue";
+const timer = ref(0);
+
+setInterval(() => timer.value++, 1000);
 </script>
