a77963910eb680d40437216d8359b77f81ec69ab
David VANMAK <vanmakdavid.lwg@gmail.com>
2023-08-01 10:59:24 +0200

    [ADD] reactive data

--- app/resources/js/Layouts/MainLayout.vue
+++ app/resources/js/Layouts/MainLayout.vue
@@ -13,5 +13,7 @@ import { Link } from "@inertiajs/vue3";
 import { ref } from "vue";
 
 const timer = ref(0);
-setInterval(() => timer.value++, 1000);
+setInterval(() => {
+    timer.value++;
+}, 1000);
 </script>
